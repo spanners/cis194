b17fc1cdfc33c37237a8ac38a95db72349a3ae12
Simon Buist <simon.buist@gmail.com>
2014-10-02 17:21:58 +0100

    Fully implement the credit card validator

--- Course/answers/01-intro.hs
+++ Course/answers/01-intro.hs
@@ -1,12 +1,31 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- Credit card validator
+
 toDigits :: Integer -> [Integer] 
 toDigits n | n <= 0 = []
            | otherwise = [ read [ch] | ch <- show n ]
 
-toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits
+toStrings :: [Integer] -> [String]
+toStrings ns = [ show ch | ch <- ns ] 
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = zipWith (*) $ cycle [1,2]
+doubleEveryOther = zipWith (*) (cycle [1,2])
+
+toInteger_ :: String -> Integer
+toInteger_ s = read s
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . toDigits . toInteger_ . concat . toStrings
+
+validate :: Integer -> Bool
+validate = (==0) . (`mod` 10) . sumDigits . doubleEveryOther . toDigits
+
+eachLine :: (String -> String) -> (String -> String)
+eachLine f = unlines . map f . lines
+
+validator :: String -> String
+validator = show . validate . read
 
 main :: IO ()
-main = undefined
+main = interact (eachLine validator)
