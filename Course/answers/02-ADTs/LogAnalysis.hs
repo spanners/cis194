cc99644ff65e36d6d1ab8e524d2f63e29a863d3d
Simon Buist <simon.buist@gmail.com>
2014-10-07 10:00:10 +0100

    Fancy formatting

--- Course/answers/02-ADTs/LogAnalysis.hs
+++ Course/answers/02-ADTs/LogAnalysis.hs
@@ -4,46 +4,41 @@ module LogAnalysis where
 
 import Log
 
-{- TODO 
-Could use serious cleaning up with
-`take`, `drop`, (.)
--}
-
 parseMessage :: String -> LogMessage
 parseMessage m = parseHelper $ words m
 
 parseHelper :: [String] -> LogMessage
 parseHelper wrds = case (head wrds) of
-               "I" -> LogMessage Info (bar wrds) (baz wrds)
-               "W" -> LogMessage Warning (bar wrds) (baz wrds)
-               "E" -> LogMessage (Error $ bar wrds) (bar $ tail wrds) (baz $ tail wrds)
-               _   -> Unknown $ unwords wrds
-
-bar :: [String] -> Int
-bar = read . head . tail
-
-baz :: [String] -> String
-baz = unwords . drop 2
+    "I" -> LogMessage Info               (lvl wrds)        (msg wrds)
+    "W" -> LogMessage Warning            (lvl wrds)        (msg wrds)
+    "E" -> LogMessage (Error $ lvl wrds) (lvl $ tail wrds) (msg $ tail wrds)
+    _   -> Unknown $ unwords wrds
+  where lvl :: [String] -> Int
+        lvl = read . head . tail
+        msg :: [String] -> String
+        msg = unwords . drop 2
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (Unknown _)            t                                           = t
-insert msg@(LogMessage _ _ _) Leaf                                        = Node Leaf msg Leaf
-insert msg@(LogMessage _ _ _) (Node l lm r) | getLevel msg < getLevel lm  = Node (insert msg l) lm r
-                                            | getLevel msg >= getLevel lm = Node l lm (insert msg r)
-insert (LogMessage _ _ _)     (Node _ _ _)                                = error "Fell off the face of the earth"
+insert (Unknown _)         t        = t
+insert msg                 Leaf     = Node Leaf msg Leaf
+insert msg                (Node l lm r) 
+    | getLevel msg < getLevel lm    = Node (insert msg l) lm r
+    | getLevel msg >= getLevel lm   = Node l lm (insert msg r)
+insert (LogMessage _ _ _) (Node _ _  _)
+                                    = error "We should not get here"
 
 getLevel :: LogMessage -> Int
 getLevel (LogMessage _ l _) = l
-getLevel (Unknown _) = error "LogMessage Unknown has no level"
+getLevel (Unknown _)        = error "LogMessage Unknown has no level"
 
 build :: [LogMessage] -> MessageTree
 build = foldr insert Leaf
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder Leaf = []
+inOrder Leaf          = []
 inOrder (Node l lm r) = inOrder l ++ [lm] ++ inOrder r
 
 whatWentWrong :: [LogMessage] -> [String]
