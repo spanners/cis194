db944b65648545e3845796ec3744e38b68f36c87
Simon Buist <simon.buist@gmail.com>
2014-10-07 17:16:13 +0100

    Implement a very hacky solution, found in `nearlyThere`

--- Course/answers/03-rec-poly/Histogram.hs
+++ Course/answers/03-rec-poly/Histogram.hs
@@ -6,11 +6,25 @@ module Histogram where
 import Data.Map (toList, fromListWith)
 import Data.List (maximumBy)
 
+nearlyThere :: [Integer] -> [String]
+nearlyThere = addNewlines . doThat . doIt
 
-skim :: [String] -> [[String]]
-skim foo = map (\i -> replicate (abs $ len foo - length i) " \n") foo
+doThat :: forall a. [([a], [a])] -> [[a]]
+doThat = map (\(a,b) -> a ++ b)
+
+doIt :: [Integer] -> [(String, String)]
+doIt bar = zip (skim $ histogram bar) (histogram bar)
+
+skim :: [String] -> [String]
+skim foo = map (concat . (\i -> replicate (abs $ len foo - length i) " ")) foo
 -- GHCi> zip (map (\i -> concat $ replicate (abs $ len bar - length i) " \n") bar) bar
 -- > [("","aaa"),(" \n","bb"),(" \n \n","c")]
+
+addNewlines :: [String] -> [String]
+addNewlines = map (intersperseString "\n")
+
+intersperseString :: forall b. [b] -> [b] -> [b]
+intersperseString = ((=<<) . flip (:))
 
 len :: [String] -> Int
 len = fst . maxFst . zipLengths
@@ -30,7 +44,7 @@ freq input = toList $ fromListWith (+) [(c, 1) | c <- input]
 -- IDEA: don't build the tower here, instead do it as the last step and use
 -- `Data.List.intercalate`
 tower :: (a, Integer) -> String
-tower (_, frq) = concat ["*\n" | _ <- [0..frq]]
+tower (_, frq) = concat ["*" | _ <- [0..frq]]
 
 main :: IO ()
 main = undefined
