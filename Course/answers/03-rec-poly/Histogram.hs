5c1112aebcb4a67b60858bae637823543a9e928a
Simon Buist <simon.buist@gmail.com>
2014-10-07 15:42:33 +0100

    Progress made on the Histogram. May need serious refactoring

--- Course/answers/03-rec-poly/Histogram.hs
+++ Course/answers/03-rec-poly/Histogram.hs
@@ -3,14 +3,32 @@
 
 module Histogram where
 
-import qualified Data.Map as D
+import Data.Map (toList, fromListWith)
+import Data.List (maximumBy)
+
+
+skim :: [String] -> [[String]]
+skim foo = map (\i -> replicate (abs $ len foo - length i) " \n") foo
+-- GHCi> zip (map (\i -> concat $ replicate (abs $ len bar - length i) " \n") bar) bar
+-- > [("","aaa"),(" \n","bb"),(" \n \n","c")]
+
+len :: [String] -> Int
+len = fst . maxFst . zipLengths
+
+maxFst :: [(Int, String)] -> (Int, String) 
+maxFst = maximumBy (\a b -> if fst a > fst b then GT else LT)
+
+zipLengths :: [String] -> [(Int, String)]
+zipLengths xs = zip (map length xs) xs
 
 histogram :: [Integer] -> [String]
 histogram = map tower . freq
 
 freq :: forall k a. (Ord k, Num a) => [k] -> [(k, a)]
-freq input = D.toList $ D.fromListWith (+) [(c, 1) | c <- input]
+freq input = toList $ fromListWith (+) [(c, 1) | c <- input]
 
+-- IDEA: don't build the tower here, instead do it as the last step and use
+-- `Data.List.intercalate`
 tower :: (a, Integer) -> String
 tower (_, frq) = concat ["*\n" | _ <- [0..frq]]
 
