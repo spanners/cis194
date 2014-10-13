72ef5298ed461450f26984e761e24263b47d97a5
Simon Buist <simon.buist@gmail.com>
2014-10-13 12:27:50 +0100

    Clean up function

--- Course/answers/03-rec-poly/Histogram.cheat.hs
+++ Course/answers/03-rec-poly/Histogram.cheat.hs
@@ -1,9 +1,9 @@
 {-# OPTIONS_GHC -Wall #-}
 module Histogram where
-import Data.List
+import Data.List (intersperse, group, transpose, sort)
 histogram :: [Integer] -> String
-histogram xs = (concat . map (++ "\n") $ transpose $ map pad groups) 
-    ++ "==========\n0123456789\n"
+histogram xs = (concat $ intersperse "\n" $ transpose $ map pad groups) 
+    ++ "\n==========\n0123456789\n"
     where
     groups = map tail $ group $ sort $ [0..9] ++ xs
     height = maximum $ map length groups
