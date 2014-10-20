56b354ec8d4759fe3a0efb556d2f8425fe7d59ab
U-CODESTONE\Simon Buist <Simon Buist@WS-001.codestone.net>
2014-10-20 16:55:54 +0100

    HLint refactors

--- Course/answers/03-rec-poly/Histogram.hs
+++ Course/answers/03-rec-poly/Histogram.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module Histogram where
-import Data.List (intersperse, group, transpose, sort)
+import Data.List (intercalate, group, transpose, sort)
 histogram :: [Integer] -> String
-histogram xs = (concat $ intersperse "\n" $ transpose $ map pad groups) 
+histogram xs = intercalate "\n" (transpose $ map pad groups) 
     ++ "\n==========\n0123456789\n"
     where
     groups = map tail $ group $ sort $ [0..9] ++ xs
--- Course/answers/04-higher-order/fun1.hs
+++ Course/answers/04-higher-order/fun1.hs
@@ -8,11 +8,11 @@ import Data.List (foldl')
 fun1 :: [Integer] -> Integer
 fun1 = foldl' f 1
   where
-    f :: Integer -> (Integer -> Integer)
+    f :: Integer -> Integer -> Integer
     f x = if even x then ((x - 2)*) else id
 
 fun2 :: Integer -> Integer
 fun2 = sum . filter even . takeWhile (>1) . iterate collatz 
   where
     collatz :: Integer -> Integer
-    collatz n = if even n then (n `div` 2) else (3 * n + 1)
+    collatz n = if even n then n `div` 2 else 3 * n + 1
