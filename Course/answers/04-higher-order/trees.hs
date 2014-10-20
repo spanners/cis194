9d70bff8808fbf35ed1e62f136f269631af69c22
U-CODESTONE\Simon Buist <Simon Buist@WS-001.codestone.net>
2014-10-20 17:31:25 +0100

    Add some bunk instance of F.Foldable

--- Course/answers/04-higher-order/trees.hs
+++ Course/answers/04-higher-order/trees.hs
@@ -1,8 +1,18 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# UnicodeSyntax #-}
 
 module Tree where
+
+import Data.Foldable as F
+import Data.Monoid
 
 data Tree a = Leaf | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
 
+foldTree :: [a] -> Tree a
+foldTree = undefined
+
+instance F.Foldable Tree where
+    foldMap f Leaf = mempty
+    foldMap f (Node i l e r) = undefined
