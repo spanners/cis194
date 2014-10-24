dc2d610e376133eef1469db3af2336b892f8745d
Simon Buist <simon.buist@gmail.com>
2014-10-24 14:45:03 +0100

    Implement sieve of sundaram

--- Course/answers/04-higher-order/sieveSundaram.hs
+++ Course/answers/04-higher-order/sieveSundaram.hs
@@ -1,11 +1,14 @@
 {-# OPTIONS_GHC -Wall #-}
 
--- look at 
--- https://stackoverflow.com/questions/16246456/sieve-of-sundaram-list-comprehension
-
 module SieveSundaram where
 
+cartProd :: [a] -> [(a, a)]
+cartProd xs = [(x,y) | x <- xs, y <- xs]
+
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram = map (\x -> 2 * x + 1) . filter f . enumFromTo 1 
-    where
-      f x = all (\(i,j) -> i + j + 2 * i * j > x) [(i,j) | i <- [1..x], j <- [1..x], i <= j]
+sieveSundaram = map ((+1) . (*2)) . filter valid 
+                                  . enumFromTo 1
+
+valid :: Integer -> Bool
+valid x = not . any (\(i,j) -> i + j + 2*i*j == x) 
+              . filter (uncurry (<=)) $ cartProd [1..x]
