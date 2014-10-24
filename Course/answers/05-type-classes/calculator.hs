ad1019982658b5d8db6aab0debbbe10433d9a1be
Simon Buist <simon.buist@gmail.com>
2014-10-24 22:30:16 +0100

    Add class Expr and instances of Expr (ExprT, Integer, Bool)

--- Course/answers/05-type-classes/calculator.hs
+++ Course/answers/05-type-classes/calculator.hs
@@ -4,7 +4,6 @@ module Calculator where
 
 import ExprT(ExprT(..))
 import Parser(parseExp)
-import Control.Applicative
 
 eval :: ExprT -> Integer
 eval (Lit n)   = n
@@ -12,8 +11,27 @@ eval (Mul x y) = eval x * eval y
 eval (Add x y) = eval x + eval y
 
 evalStr :: String -> Maybe Integer
-evalStr s = eval <$> parseExp Lit Add Mul s
+evalStr = fmap eval . parseExp Lit Add Mul
 
 class Expr a where
-  lit :: a -> ExprT
-  add, mul :: a -> a -> ExprT
+    lit :: Integer -> a
+    mul :: a -> a -> a
+    add :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit
+    add = Add
+    mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (>0)
+    add = (||)
+    mul = (&&)
