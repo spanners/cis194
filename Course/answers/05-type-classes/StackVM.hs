b34e8f9e6e5e349c038e19d3845b0e403f6f7a1b
Simon Buist <simon.buist@gmail.com>
2014-10-29 15:21:39 +0000

    Heavily reformat the `execute` function

--- Course/answers/05-type-classes/StackVM.hs
+++ Course/answers/05-type-classes/StackVM.hs
@@ -29,30 +29,22 @@ errUnderflow op = Left $ "Stack underflow with '" ++ op ++ "' opcode."
 
 -- Execute a program against a given stack.
 execute :: Stack -> Program -> Either String StackVal
-execute [] []                               = Right Void
-execute (s:_) []                            = Right s
+execute []                       []             = Right Void
+execute (s       : _           ) []             = Right s
+execute s                        (PushI x : xs) = execute (IVal x : s ) xs
+execute s                        (PushB x : xs) = execute (BVal x : s ) xs
+execute (IVal s1 : IVal s2 : ss) (Add     : xs) = execute (s'     : ss) xs where s' = IVal (s1 + s2)
+execute (_       : _       : _ ) (Add     : _ ) = errType "Add"
+execute _                        (Add     : _ ) = errUnderflow "Add"
+execute (IVal s1 : IVal s2 : ss) (Mul     : xs) = execute (s'     : ss) xs where s' = IVal (s1 * s2)
+execute (_       : _       : _ ) (Mul     : _ ) = errType "Mul"
+execute _                        (Mul     : _ ) = errUnderflow "Mul"
+execute (BVal s1 : BVal s2 : ss) (And     : xs) = execute (s'     : ss) xs where s' = BVal (s1 && s2)
+execute (_       : _       : _ ) (And     : _ ) = errType "And"
+execute _                        (And     : _ ) = errUnderflow "And"
+execute (BVal s1 : BVal s2 : ss) (Or      : xs) = execute (s'     : ss) xs where s' = BVal (s1 || s2)
+execute (_      : _       : _  ) (Or      : _ ) = errType "Or"
+execute _                        (Or      : _ ) = errUnderflow "Or"
 
-execute s (PushI x : xs)                    = execute (IVal x : s) xs
-execute s (PushB x : xs)                    = execute (BVal x : s) xs
-
-execute (IVal s1 : IVal s2 : ss) (Add : xs) = execute (s':ss) xs
-    where s' = IVal (s1 + s2)
-execute (_:_:_) (Add:_)                     = errType "Add"
-execute _ (Add:_)                           = errUnderflow "Add"
-
-execute (IVal s1:IVal s2:ss) (Mul : xs)     = execute (s':ss) xs
-    where s' = IVal (s1 * s2)
-execute (_:_:_) (Mul:_)                     = errType "Mul"
-execute _ (Mul:_)                           = errUnderflow "Mul"
-
-execute (BVal s1:BVal s2:ss) (And : xs)     = execute (s':ss) xs
-    where s' = BVal (s1 && s2)
-execute (_:_:_) (And:_)                     = errType "And"
-execute _ (And:_)                           = errUnderflow "And"
-
-execute (BVal s1 : BVal s2 : ss) (Or : xs)  = execute (s':ss) xs
-    where s' = BVal (s1 || s2)
-execute (_:_:_) (Or:_)                      = errType "Or"
-execute _ (Or:_)                            = errUnderflow "Or"
-
+test :: Either String StackVal
 test = stackVM [PushI 3, PushI 5, Add]
