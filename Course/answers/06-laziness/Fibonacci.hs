caf367ef4876851e08db54d72247fe94ad557c15
Simon Buist <simon.buist@gmail.com>
2014-10-30 21:39:41 +0000

    Use UnicodeSyntax for pretty source code!

--- Course/answers/06-laziness/Fibonacci.hs
+++ Course/answers/06-laziness/Fibonacci.hs
@@ -1,46 +1,47 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UnicodeSyntax #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Fibonacci where
 
-fib :: Integer -> Integer
+fib ∷ Integer → Integer
 fib = round . (/ sq5) . (phi **) . fromIntegral
-  where sq5 = sqrt 5 :: Double
+  where sq5 = sqrt 5 ∷ Double
         phi = (1 + sq5) / 2
 
-fib1 :: [Integer]
-fib1 = map fib [0..]
+fibs1 ∷ [Integer]
+fibs1 = map fib [0..]
 
 data Stream a = SCons a (Stream a)
 
-streamToList :: Stream a -> [a]
+streamToList ∷ Stream a → [a]
 streamToList (SCons x xs) = x : streamToList xs
 
-instance Show a => Show (Stream a) where
+instance Show a ⇒ Show (Stream a) where
     show = show . take 20 . streamToList
 
-streamRepeat :: a -> Stream a
+streamRepeat ∷ a → Stream a
 streamRepeat x = SCons x (streamRepeat x)
 
-streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap ∷ (a → b) → Stream a → Stream b
 streamMap f (SCons x xs) = SCons (f x) (streamMap f xs)
 
-streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed ∷ (a → a) → a → Stream a
 streamFromSeed f x = SCons (f x) (streamFromSeed f (f x))
 
-nats :: Stream Integer
+nats ∷ Stream Integer
 nats = streamFromSeed (+1) (-1)
 
-interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams ∷ Stream a → Stream a → Stream a
 interleaveStreams (SCons x s1) s2 = SCons x (interleaveStreams s2 s1)
 
-ruler :: Stream Integer
+ruler ∷ Stream Integer
 ruler = foldr1 interleaveStreams (map streamRepeat [0..])
 
-x' :: Stream Integer
+x' ∷ Stream Integer
 x' = SCons 0 (SCons 1 (streamRepeat 0))
 
-streamZipWith :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+streamZipWith ∷ (a → b → c) → Stream a → Stream b → Stream c
 streamZipWith f (SCons x xs) (SCons y ys) = SCons (f x y) (streamZipWith f xs ys)
 
 instance Num (Stream Integer) where
