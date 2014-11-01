0a4f3ab0667ddf2804be74543d316914133d1080
Simon Buist <simon.buist@gmail.com>
2014-11-01 16:03:13 +0000

    Add expression folder

--- Course/answers/05-type-classes/calculator.hs
+++ Course/answers/05-type-classes/calculator.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances, FlexibleInstances #-}
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances, FlexibleInstances, UnicodeSyntax #-}
 
 module Calculator where
 
@@ -7,25 +7,25 @@ import ExprT as E
 import Parser (parseExp)
 import StackVM as S
 
-eval :: ExprT -> Integer
+eval ∷ ExprT → Integer
 eval (E.Lit n)   = n
 eval (E.Mul x y) = eval x * eval y
 eval (E.Add x y) = eval x + eval y
 
-evalStr :: String -> Maybe Integer
+evalStr ∷ String → Maybe Integer
 evalStr = fmap eval . parseExp E.Lit E.Add E.Mul
 
 class Expr a where
-    lit :: Integer -> a
-    mul :: a -> a -> a
-    add :: a -> a -> a
+    lit ∷ Integer → a
+    mul ∷ a → a → a
+    add ∷ a → a → a
 
 instance Expr ExprT where
     lit = E.Lit
     add = E.Add
     mul = E.Mul
 
-reify :: ExprT -> ExprT
+reify ∷ ExprT → ExprT
 reify = id
 
 instance Expr Integer where
@@ -52,19 +52,19 @@ instance Expr Mod7 where
     mul i j = (i * j) `mod` 7
 
 
-testExp :: Expr a => Maybe a
+testExp ∷ Expr a ⇒ Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
 
-testInteger :: Maybe Integer
+testInteger ∷ Maybe Integer
 testInteger = testExp
 
-testBool :: Maybe Bool
+testBool ∷ Maybe Bool
 testBool = testExp
 
-testMM :: Maybe MinMax
+testMM ∷ Maybe MinMax
 testMM = testExp 
 
-testSat :: Maybe Mod7
+testSat ∷ Maybe Mod7
 testSat = testExp
 
 instance Expr Program where
@@ -72,8 +72,19 @@ instance Expr Program where
     add a b = a ++ b ++ [S.Mul]
     mul a b = a ++ b ++ [S.Add]
 
-compile :: String -> Maybe S.Program
+compile ∷ String → Maybe S.Program
 compile = parseExp lit add mul
 
-evalCode :: String -> Maybe (Either String S.StackVal)
+evalCode ∷ String → Maybe (Either String S.StackVal)
 evalCode = fmap stackVM . parseExp lit add mul
+
+exprTFold ∷ (Integer → t) → (t → t → t) → (t → t → t) → ExprT → t
+exprTFold f _ _ (E.Lit i)     = f i
+exprTFold f g h (E.Add e1 e2) = g (exprTFold f g h e1) (exprTFold f g h e2)
+exprTFold f g h (E.Mul e1 e2) = h (exprTFold f g h e1) (exprTFold f g h e2)
+
+eval2 ∷ ExprT → Integer
+eval2 = exprTFold id (+) (*)
+
+numLiterals ∷ ExprT → Integer
+numLiterals = exprTFold (const 1) (+) (+)
