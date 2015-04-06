7788fd861c22b4af0dec36b8ecc899b4af577c95
Simon Buist <simon.buist@gmail.com>
2015-04-06 23:43:35 +0100

    Add simple example of a joinList. `simple`

--- Course/answers/07-folds-monoids/JoinList.hs
+++ Course/answers/07-folds-monoids/JoinList.hs
@@ -20,6 +20,13 @@ data JoinList m a = Empty
 instance (Arbitrary a, Sized a) => Arbitrary (JoinList Size a) where
   arbitrary = sized jTree'
 
+jTree' :: (Arbitrary a1, Arbitrary a2) => Int -> Gen (JoinList a1 a2)
+jTree' 0 = liftM2 Single arbitrary arbitrary
+jTree' n | n>0 =
+    oneof [ liftM2 Single arbitrary arbitrary
+          , liftM3 Append arbitrary subtree subtree ]
+      where subtree = jTree' (n `div` 2)
+
 tag :: Monoid m => JoinList m a -> m
 tag Empty = mempty
 tag (Single m _) = m
@@ -37,38 +44,48 @@ example = Append (Product 210)
                (Single (Product 3) 'a')))
            (Single (Product 7) 'h')
 
+simple :: JoinList (Sum Integer) Char
+simple = Append (Sum 5) (Single (Sum 2) 'a') (Single (Sum 3) 'b')
+
 sizeJ :: (Sized b, Monoid b) => JoinList b a -> Int
 sizeJ = getSize . size . tag
-
-indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ _ Empty        = Nothing
-indexJ 0 (Single _ a) = Just a
-indexJ _ (Single _ _) = Nothing
-indexJ n node@(Append _ jll jlr)
-  | n >= sizeJ node = Nothing
-  | n >= sizeJ jll  = indexJ (n - sizeJ jll) jlr
-  | otherwise       = indexJ n jll
 
 jlToList :: JoinList m a -> [a]
 jlToList Empty            = []
 jlToList (Single _ a)     = [a]
 jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 
-(!!?) :: [a] -> Int -> Maybe a
-[] !!? _ = Nothing
-_ !!? i | i < 0 = Nothing
-(x:xs) !!? 0 = Just x
-(x:xs) !!? i = xs !!? (i-1)
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty  = Nothing
+indexJ i (Single m d)
+    | i == 0    = Just d
+    | otherwise = Nothing
+indexJ i (Append m jL1 jL2)
+    | i < 0     = Nothing
+    | i >= getSize (size m) = Nothing     -- optional, more efficient to have it
+    | i < s1    = indexJ i jL1
+    | otherwise = indexJ (i - s1) jL2
+      where
+        s1 = getSize . size $ tag jL1
 
-jTree' :: (Arbitrary a1, Arbitrary a2) => Int -> Gen (JoinList a1 a2)
-jTree' 0 = liftM2 Single arbitrary arbitrary
-jTree' n | n>0 =
-    oneof [ liftM2 Single arbitrary arbitrary
-          , liftM3 Append arbitrary subtree subtree ]
-      where subtree = jTree' (n `div` 2)
+(!!?) :: Int -> [a] -> Maybe a
+_ !!? []        = Nothing
+i !!? _ | i < 0 = Nothing
+0 !!? (x:xs)    = Just x
+i !!? (x:xs)    = (i-1) !!? xs
 
 prop_indexJ :: Int -> (JoinList Size Size) -> Bool
-prop_indexJ i jl = (indexJ i jl) == (jlToList jl !!? i)
+prop_indexJ i jl = (indexJ i jl) == (i !!? jlToList jl)
+
+{- This fails with the following inputs
+ - 0
+ - Append (Size 1) (Single (Size 0) (Size 0)) (Single (Size (-1)) (Size 1)) -}
+
+{- WHY? 
+ - 
+ - 
+ -
+ - -}
 
 runTests :: IO Bool
 runTests = $(quickCheckAll)
