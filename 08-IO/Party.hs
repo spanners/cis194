ebbd337a0e4e874ecee013d031cb9294375cc7a2
Simon Buist <simon.buist@gmail.com>
2015-04-25 16:41:05 +0100

    Implement all of HW08

--- 08-IO/Party.hs
+++ 08-IO/Party.hs
@@ -2,6 +2,8 @@ module Party where
 
 import Employee
 import Data.Monoid
+import Data.Tree
+import Data.List
 
 glCons :: Employee ->GuestList -> GuestList
 glCons e (GL l n) = (GL  (e : l) (n + empFun e))
@@ -12,3 +14,24 @@ instance Monoid GuestList where
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
+
+treeFold :: (a -> [b] -> b) -> [b] -> Tree a -> b
+treeFold f l (Node a ts) = f a l'
+  where l' = map (treeFold f l) ts
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel b gls = (bestWith, bestWithout)
+  where bestWith = glCons b $ mconcat $ map snd gls
+        bestWithout = mconcat $ map (uncurry moreFun) gls
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel []
+
+formatGuestList :: GuestList -> [String]
+formatGuestList (GL emps fun) = ("Total fun: " ++ show fun) : sort (map empName emps)
+
+main :: IO ()
+main = do
+  company <- readFile "company.txt"
+  let party = maxFun (read company :: Tree Employee)
+  mapM_ putStrLn (formatGuestList party)
